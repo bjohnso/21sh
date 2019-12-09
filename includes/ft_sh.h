11741fc183c8e98dc72ed27caf831b09d29b781d
brandon JOHNSON <bjohnso@c3r1s6.wethinkcode.co.za>
2019-12-09 19:35:42 +0200

    Added Shell Loop

--- includes/ft_sh.h
+++ includes/ft_sh.h
@@ -3,4 +3,23 @@
 
 #include "libft.h"
 
+typedef struct      s_shell
+{
+    bool    quit;
+    char    **argv;
+    char    **env;
+    char    **localenv;
+    char    *prompt;
+    int     argc;
+}                   t_shell;
+
+typedef struct      s_tokenlist
+{
+    char                type[3];
+    int                 len;
+    struct s_tokenlist  *next;
+    struct s_tokenlist  *last;
+    void                *content;
+}                       t_tokenlist;
+
 #endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
 #include "ft_sh"
 
 int     main(int argc, char **argv){
-    
+    t_shell     *shell;
+
+    while (1){
+        if (shell->quit == true)
+        break ;
+    }
 }
