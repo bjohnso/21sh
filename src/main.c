6f43286b8667242142dd039566a35f031ca35576
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-12-23 21:42:30 +0200

    Completed Parser and lexer prototype

--- src/main.c
+++ src/main.c
@@ -3,8 +3,11 @@
 int     main(int argc, char **argv){
     t_shell     *shell;
 
-    while (1){
+    /*while (1){
         if (shell->quit == true)
         break ;
-    }
+    }*/
+
+    
+
 }
--- src/shell/parser.c
+++ src/shell/parser.c
@@ -9,32 +9,31 @@ static const char   *keywords[6] = {
     "echo"
 }; 
 
-t_token     *lexer(char *str){
-    bool delim = false;
-    char *str;
-    char *lexeme;
+char     *lexer(char *str){
+    char    *look = *str;
+    char    *lexeme;
+    int     len = 0;
 
-    while (*str){
-        if (ft_isalpha(*str) || ft_isdigit(*str)){
-            
-            //Loop through till delimit
-        } else if (ft_isdigit(*str)) {
-            //Loop through till delimit
-        } else if (){
-            //Break;
+    while (*look){
+        if (*look == ' '){
+            lexeme = (char*)malloc(sizeof(char) * len);
+            ft_strncpy(lexeme, str, len);
+            str += len;
+            len = 0;
+            return lexeme;
+        } else {
+            len++;
         }
-
-        str++;
+        look++;
     }
 }
 
-t_token         *new_token(char *lexeme, char *type, int pos){
+t_token         *new_token(char *lexeme, int pos){
     t_token     *token;
     
     token = (t_token *)malloc(sizeof(t_token));
 
     token->lexeme = lexeme;
-    token->type = type;
     token->pos = pos;
 
     return token;
@@ -42,12 +41,17 @@ t_token         *new_token(char *lexeme, char *type, int pos){
 
 t_token_list    *parser(char *str){
 
-    t_token_list *token_list;
-    t_token      *token;
-    char         *lexeme = (char *)malloc(sizeof(char));
+    t_token_list    *token_list = (t_token_list *)malloc(sizeof(t_token_list));
+    t_token         *token;
+    char            *lexeme = (char *)malloc(sizeof(char));
+    int             pos = 0;
 
-    while((token = lexer)){
-        
+    while(*str){
+        token = new_token(lexer(str), pos);
+        ft_realloc(token_list->token_list, pos, pos+1);
+        token_list->token_list[pos] = token;
+        pos++;
     }
 
+    return token_list;
 }
