6ab0d62223064fcdb8a7bb9b6159cd945f0d847b
brandon JOHNSON <bjohnso@c3r3s1.wethinkcode.co.za>
2020-01-15 11:15:03 +0200

    Final clean for submission to vogsphere

--- init.sh
+++
@@ -1,2 +0,0 @@
-gcc memory_checker/memory_hooks.c -o memory_checker/memory_hooks.so -fPIC -shared -ldl -D_GNU_SOURCE
-export LD_PRELOAD="/home/nullfinder/CProjects/Minishell/memory_checker/memory_hooks.so"
